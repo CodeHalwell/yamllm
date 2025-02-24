1a3c8e82faa570658766d4da67bdd617f8999800
Daniel Halwell <danielhalwell@gmail.com>
2025-02-24 16:38:21 +0000

    Bump version to 0.1.9 and update package name in pyproject.toml

--- yamllm/__init__.py
+++ yamllm/__init__.py
@@ -5,7 +5,7 @@ from .core.config import Config
 from .memory.conversation_store import ConversationStore, VectorStore
 from .tools import *
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 __all__ = [
     "LLM",
